1bcc3951ebc820e6c72ce29263366a1fb7216728
Andre Rodrigues <andre.silva96@live.com>
2017-09-12 22:47:16 -0300

    configurando cache

--- src/doctrine.php
+++ src/doctrine.php
@@ -1,7 +1,8 @@
 <?php
 
-use Doctrine\ORM\Tools\Setup;   // anotacoes
 use Doctrine\ORM\EntityManager; // uso das entidades
+use Doctrine\Common\Cache\ArrayCache;   // cache
+use Doctrine\ORM\Configuration;
 
 $paths = [
     __DIR__ . '/Entity'
@@ -9,6 +10,8 @@ $paths = [
 
 // Modo de desenvolvimento
 $isDev = true;
+
+$cache = new ArrayCache();
 
 // Configuracoes do BD
 $dbParams = [
@@ -19,7 +22,15 @@ $dbParams = [
 ];
 
 // Configuracao do Setup
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDev);
+$config = new Configuration();
+$config->setMetadataCacheImpl($cache);
+$driverImpl = $config->newDefaultAnnotationDriver($paths);
+$config->setMetadataDriverImpl($driverImpl);
+$config->setQueryCacheImpl($cache);
+$config->setProxyDir(__DIR__ . '/../proxy');
+$config->setProxyNamespace('App\Proxies');
+//deixar false em producao pq em dev ira gerar o proxy toda hora quando a gente alterar uma entidade
+$config->setAutoGenerateProxyClasses(true);
 
 $entityManager = EntityManager::create($dbParams, $config);
 
