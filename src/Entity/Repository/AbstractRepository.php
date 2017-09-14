b8bcc50d9683511672e8deaf1f11a725c0cf2d3f
Andre Rodrigues <andre.silva96@live.com>
2017-09-13 22:28:12 -0300

    Repository

---
+++ src/Entity/Repository/AbstractRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+use Doctrine\ORM\EntityRepository;
+
+abstract class AbstractRepository extends EntityRepository
+{
+    public function getReference($id,$class = null)
+    {
+        if(!$class){
+            $class = $this->getClassName();
+        }
+        return $this->getEntityManager()->getReference($class,$id);
+    }
+
+    public function save($entity)
+    {
+        //se for uma entidade nova ele insere nova
+        if($this->getEntityManager()->getUnitOfWork()->getEntityState($entity) == UnitOfWork::STATE_NEW){
+            $this->getEntityManager()->persist($entity);
+        }
+        $this->getEntityManager()->flush();
+        return $entity;
+    }
+
+    public function delete($id)
+    {
+        $entity = $this->getReference($id);
+        $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+    }
+}
